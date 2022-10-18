2f2603f23773194e2256f68ba1525dd379557717
coralsprings-tcs <115885261+coralsprings-tcs@users.noreply.github.com>
2022-10-18 17:48:55 -0400

    rev4bv4b

--- PythonCythonFiles/setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
- 
-ext_options = {"compiler_directives": {"profile": True}, "annotate": True}
- 
-setup(ext_modules = cythonize("PythonEegTopology.pyx", **ext_options)
-)
