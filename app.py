90d102498fb9117c04d58762c3f22c91fe45e9f1
Mann <mannj@mail.usf.edu>
2022-10-29 13:39:32 -0400

    Added render template and basic jinja input

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask
-
+from flask import Flask, render_template
+import datetime
 # python -m flask --app hello run is how you call flask
 # when the file being called is named app.py you just need to use "python -m flask run" when in the app's folder
 app = Flask(__name__)
@@ -9,7 +9,11 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
-# http://127.0.0.1:5000/hi gives the below
-@app.route("/hi")
-def hi():
-    return "<p>Hi!</p>"
+# http://127.0.0.1:5000/index gives the below
+@app.route("/index")
+def index():
+    return render_template('index.html', utc_dt=datetime.datetime.utcnow())
+
+@app.route("/ext_index")
+def ext_index():
+    return render_template('ext_index.html', utc_dt=datetime.datetime.utcnow())
